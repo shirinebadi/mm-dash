16b93df2680cc43c0d6f7614375e6c72ec4d0069
shirin.ebadi <shirin.ebadi@snapp.cab>
2022-06-29 14:12:11 +0430

    feat: add main page handler

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,11 @@ def check_health():
     return jsonify({
         'message': 'OK'
     }), 200
+
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('index.html') 
+
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0', port=8000)
