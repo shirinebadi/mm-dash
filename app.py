f2a0858026fe09edb837b3dddc619d70a8096491
shirin.ebadi <shirin.ebadi@snapp.cab>
2022-06-29 21:59:24 +0430

    feat: add images

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ import time
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
+UPLOAD_FOLDER = 'static/images/'
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    info = db.Column(db.Text) 
+    star = db.Column(db.Float)
+    director = db.Column(db.String(255)) 
+
+    def __repr__(self):
+        return "<Movie %r>" % self.title
 
 
 @app.route('/api/health_check', methods=['GET'])
@@ -15,7 +31,10 @@ def check_health():
 
 @app.route('/', methods=['GET'])
 def main_page():
-    return render_template('index.html')
+    movies = Movie.query.all()
+    for movie in movies:
+        movie.id = str(movie.id)
+    return render_template('index.html', movies = movies)
 
 @app.route('/api/<int:id>')
 def search_by_id():
